e0af158d63455265d62297ef8f2ac2f15dc9dd08
Charles Cochran <charlesinvents@gmail.com>
2020-02-01 20:51:06 -0500

    Update control system

--- teamcode/AutoController.java
+++ teamcode/AutoController.java
@@ -1,18 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-import org.firstinspires.ftc.robotcore.external.Func;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 import java.util.Arrays;
-import java.util.Locale;
 
 public class AutoController {
     private LinearOpMode opmode;
@@ -22,22 +18,25 @@ public class AutoController {
     private double curTime = 0;
     private double prevTime = 0;
 
-    private double yKP = 0.6;
-    private double xKP = 0.7;
-    private double thetaKP = 0.65;
+    private double xKP = 0.09;
+    private double yKP = 0.045;
+    private double thetaKP = 0.6;
+    private double minErrorX = 2.5;
+    private double minErrorY = 4;
+    private double minErrorT = Math.toRadians(8.5);
 
     private double xErrorSum = 0;
     private double yErrorSum = 0;
     private double tErrorSum = 0;
-    private double maxErrorSum = 0.45;
-    private double maxTErrorSum = 0.25;
-    private double kI = 0.0005;
-    private double thetaKI = 0.0006;
+    private double maxErrorSum = 0.4;
+    private double maxTErrorSum = 0.4;
+    private double kI = 0.000; //.002
+    private double thetaKI = 0.000; //.005
 
-    private double kD = 0.00001;
-    private double thetaKD = 0.0001;
-    private double maxDError = 0.15;
-    private double maxDErrorT = 0.15;
+    private double kD = 0.0000; //.0002
+    private double thetaKD = 0.00000; //.00015
+    private double maxDError = 0.4;
+    private double maxDErrorT = 0.3;
     private double prevErrorX = 0;
     private double prevErrorY = 0;
     private double prevErrorT = 0;
@@ -46,15 +45,19 @@ public class AutoController {
     private double defToleranceT = 0.3;
     private double defaultTimeout = 3;
 
-    private double brakingDist = 7;
-    private double turnBrakingDist = 0.5;
+    private double yBrakeDist = 14;
+    private double xBrakeDist = 10;
+    private double tBrakeDist = Math.toRadians(4.5);
 
-    private double velThreshold = 0.4;
-    private double turnVelThreshold = 0.3;
+    private double velThreshold = 20;
+    private double tVelThreshold = Math.toRadians(10);
 
     double xVel;
     double yVel;
     double tVel;
+
+    private double liftKP = 2;
+    private double liftTolerance = 0.05;
 
     public AutoController(LinearOpMode op) {
         opmode = op;
@@ -62,17 +65,17 @@ public class AutoController {
     }
 
     public void pidDrive(double x, double y, double theta, double tolerance, double tTolerance,
-                         double timeout) {
+                         double timeout, boolean shouldStop) {
+        double startTime = runtime.seconds();
         double errorX;
         double errorY;
         double errorTheta;
-        double startTime = runtime.seconds();
+        theta = Math.toRadians(theta);           //convert heading and theta tolerance from degrees
+        tTolerance = Math.toRadians(tTolerance); //to radians
         do {
             setCurTime();
             double elapsedTime = curTime - prevTime; //the time elapsed since the previous iteration
             setPrevTime();                           //is calculated
-            /*theta = theta * (Math.PI / 180);           //convert heading and theta tolerance from
-            tTolerance = tTolerance * (Math.PI / 180); //degrees to radians*/
             //current x, y and theta positions and velocities are gotten
             r.posAndVel = r.odometryThread.getPosAndVel();
             double worldErrorX = x - r.posAndVel[0]; //x and y world errors are calculated
@@ -84,21 +87,15 @@ public class AutoController {
             errorY = (worldErrorX * Math.sin(thetaOffset) + (worldErrorY * Math.cos(thetaOffset)));
             //the theta error is calculated and normalized
             errorTheta = AngleUnit.normalizeRadians(theta - r.posAndVel[2]);
+
             //if we are within a small distance of the target position in all three dimensions or
             //the timeout has been reached, exit the control loop; if not, continue
-            if ((Math.abs(errorY) < tolerance && Math.abs(errorX) < tolerance &&
-                    Math.abs(errorTheta) < tTolerance) || (curTime - startTime) >= timeout) {
+            boolean xReached = Math.abs(errorX) <= tolerance;
+            boolean yReached = Math.abs(errorY) <= tolerance;
+            boolean thetaReached = Math.abs(errorTheta) <= tTolerance;
+            if ((xReached && yReached && thetaReached) || (curTime - startTime) >= timeout) {
                 break;
             }
-
-            //the errors are scaled and clipped to a scale of -1 to 1
-            errorX = Range.clip(Range.scale(errorX, -brakingDist, brakingDist, -1, 1),
-                    -1, 1);
-            errorY = Range.clip(Range.scale(errorY, -brakingDist, brakingDist, -1, 1),
-                    -1, 1);
-            errorTheta = Range.clip(Range.scale(errorTheta, -turnBrakingDist, turnBrakingDist,
-                    -1, 1), -1, 1);
-
 
             //the error sums for each of three dimensions are updated and clipped (for the integral
             //component)
@@ -117,73 +114,84 @@ public class AutoController {
             double dErrorT = Range.clip(thetaKD * (errorTheta - prevErrorT) / elapsedTime,
                     -maxDErrorT, maxDErrorT);
 
-            //if we are near the target x position and are traveling quickly, slam on the brakes in
-            //the x dimension
+            double xP = (Math.abs(errorX) > minErrorX ? errorX : minErrorX * Math.copySign(1.0,
+                    errorX)) * xKP;
+            double yP = (Math.abs(errorY) > minErrorY ? errorY : minErrorY * Math.copySign(1.0,
+                    errorY)) * yKP;
+            double tP = (Math.abs(errorTheta) > minErrorT ? errorTheta : minErrorT * Math.copySign(
+                    1.0, errorTheta)) * thetaKP;
+
+            //if we are nearing the target x position and are traveling quickly, or if the x goal
+            //has been reached, slam on the brakes in the x dimension
             //otherwise, calculate the desired x velocity by adding the proportional, integral, and
             //derivative components together
-            if (Math.abs(errorX) < 1 && Math.abs(r.posAndVel[3]) > velThreshold) {
+            if ((Math.abs(errorX) < xBrakeDist && Math.abs(r.posAndVel[3]) > velThreshold) ||
+                    xReached) {
                 xVel = 0;
             } else {
-                xVel = (errorX * xKP) + xErrorSum + dErrorX;
+                xVel = xP + xErrorSum + dErrorX;
             }
 
-            //if we are near the target x position and are traveling quickly, slam on the brakes in
-            //the x dimension
+            //if we are nearing the target y position and are traveling quickly, or if the y goal
+            //has been reached, slam on the brakes in the y dimension
             //otherwise, calculate the desired y velocity by adding the proportional, integral, and
             //derivative components together
-            if (Math.abs(errorY) < 1 && Math.abs(r.posAndVel[4]) > velThreshold) {
+            if ((Math.abs(errorY) < yBrakeDist && Math.abs(r.posAndVel[4]) > velThreshold) ||
+                    yReached) {
                 yVel = 0;
             } else {
-                yVel = (errorY * yKP) + yErrorSum + dErrorY;
+                yVel = yP + yErrorSum + dErrorY;
             }
 
-            //if we are near the target heading and are turning quickly, slam on the brakes in the
-            //theta dimension
+            //if we are nearing the target heading and are turning quickly, or if the theta goal has
+            //been reached, slam on the brakes in the theta dimension
             //otherwise, calculate the desired theta velocity by adding the proportional, integral,
             //and derivative components together
-            if (Math.abs(errorTheta) < 1 && Math.abs(r.posAndVel[5]) > turnVelThreshold) {
+            if ((Math.abs(errorTheta) < tBrakeDist && Math.abs(r.posAndVel[5]) > tVelThreshold)
+                    || thetaReached) {
                 tVel = 0;
             } else {
-                tVel = (errorTheta * thetaKP) + tErrorSum + dErrorT;
+                tVel = tP + tErrorSum + dErrorT;
             }
-            //output the x, y and theta velocity setpoints to stage 2
-            velDrive(xVel, yVel, tVel);
+
+            //power the motors with the calculated x, y and theta velocities
+            powerMotors(xVel, yVel, tVel);
             opmode.telemetry.update();
         } while (opmode.opModeIsActive());
-        //after terminating the loop, stop the motors
-        velDrive(0, 0, 0);
-    }
-
-    public void pidDrive(double x, double y, double theta, double timeout) {
-        pidDrive(x, y, theta, defTolerance, defToleranceT, timeout);
-    }
-
-    public void pidDrive(double x, double y, double theta, double tol, double tTol) {
-        pidDrive(x, y, theta, tol, tTol, defaultTimeout);
-    }
-
-    public void pidDrive(double x, double y, double theta) {
-        pidDrive(x, y, theta, defTolerance, defToleranceT, defaultTimeout);
-    }
-
-    public void drive(double strafe, double drive, double turn, double timeout, boolean usePowMode) {
-        double startTime = runtime.seconds();
-        if (usePowMode) {
-            r.driveController.setMotorsMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        if (shouldStop) {
+            //if shouldStop is set to true, stop the motors after terminating the motors
+            powerMotors(0, 0, 0);
         }
-        do {
+    }
+
+    public void pidDrive(double x, double y, double theta, double timeout, boolean shouldStop) {
+        pidDrive(x, y, theta, defTolerance, defToleranceT, timeout, shouldStop);
+    }
+
+    public void pidDrive(double x, double y, double theta, double tol, double tTol, boolean shouldStop) {
+        pidDrive(x, y, theta, tol, tTol, defaultTimeout, shouldStop);
+    }
+
+    public void pidDrive(double x, double y, double theta, boolean shouldStop) {
+        pidDrive(x, y, theta, defTolerance, defToleranceT, defaultTimeout, shouldStop);
+    }
+
+    public void powDrive(double strafe, double drive, double turn, double timeout, boolean shouldStop) {
+        double startTime = runtime.seconds();
+        setMotorsMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        powerMotors(strafe, drive, turn);
+        setCurTime();
+        while (opmode.opModeIsActive() && curTime - startTime < timeout) {
+            waitFor(5);
             setCurTime();
-            r.driveController.powerMotors(strafe, drive, turn);
-        } while (opmode.opModeIsActive() && curTime - startTime < timeout);
-        r.driveController.powerMotors(0, 0, 0);
-        r.driveController.setMotorsMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+        setMotorsMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        if (shouldStop) {
+            powerMotors(0, 0, 0);
+        }
     }
 
-    public void drive (double strafe, double drive, double turn, double timeout) {
-        drive(strafe, drive, turn, timeout, true);
-    }
-
-    public void velDrive(double strafeVel, double driveVel, double turnVel) {
+    public void powerMotors(double strafeVel, double driveVel, double turnVel) {
         /*driveVel = 0;
         strafeVel = 0;*/
         //turnVel = 0;
